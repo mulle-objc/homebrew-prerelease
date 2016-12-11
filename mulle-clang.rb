0cd7f6aae1198142354b9496829e051181d95c33
Nat! <nat@mulle-kybernetik.com>
2016-12-11 12:30:03 +0100

    updated compiler to 3.9.0.2

--- mulle-clang.rb
+++ mulle-clang.rb
@@ -1,25 +1,25 @@
 class MulleClang < Formula
    homepage "https://github.com/Codeon-GmbH/mulle-clang"
    desc "Objective-C compiler for the mulle-objc runtime"
-   url "https://github.com/Codeon-GmbH/mulle-clang/archive/3.9.0.1.tar.gz"
-   sha256 "59eccdf8ae96a449ca57a6a78e7e7afc259ebe6147e60176444effb98d3349a1"
+   url "https://github.com/Codeon-GmbH/mulle-clang/archive/3.9.0.2.tar.gz"
+   sha256 "c53a6a73cc182349ae3b687840033cbfd5c4843d6ac9f89f420ece5627cb9e08"
 #   sha256 "a59bf02dbb6810ea546e9f4cb99adee0a271b58954a1ed825a393d411a719bf1"
 
 # use brew install --build-bottle mulle-clang
-   bottle do
+#   bottle do
 #    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
-      root_url "http://download.codeon.de/bottles"
-      rebuild 1
-      cellar :any
-      sha256 "3488379a7e6751c74b1c8c9dca5a7eb4e893c65fb0fdb0d735b987d7cb1d5f34" => :yosemite
-      sha256 "fb599697410cc494377ef5b55d471295c1b5469b86f1acefc663581be601ed87" => :el_capitan
-      sha256 "574d7823f3c60943b9db88c140cdfcae79096199e221969baec934de1aeb5e98" => :sierra
-   end
+#      root_url "http://download.codeon.de/bottles"
+#      rebuild 1
+#      cellar :any
+#      sha256 "3488379a7e6751c74b1c8c9dca5a7eb4e893c65fb0fdb0d735b987d7cb1d5f34" => :yosemite
+#      sha256 "fb599697410cc494377ef5b55d471295c1b5469b86f1acefc663581be601ed87" => :el_capitan
+#      sha256 "574d7823f3c60943b9db88c140cdfcae79096199e221969baec934de1aeb5e98" => :sierra
+#   end
 
    # actually depends on llvm39, but versioning is tricky in homebrew
    # probably need to change PATH below too, when llvm moves to 40
 
-   depends_on 'llvm'  => [ "with-libcxx", :build ]
+   depends_on 'llvm'  => :build
    depends_on 'cmake' => :build
 
    #
