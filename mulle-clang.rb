68473c0935b44747363fe93f41b9b854ff774d3f
Nat! <nat@codeon.de>
2017-09-15 11:22:11 +0200

    update brew formula to 5.0.0.0 (w/o lldb support)

--- mulle-clang.rb
+++ mulle-clang.rb
@@ -6,13 +6,12 @@ class MulleClang < Formula
 #
 #    1. Create a release on github
 #    2. Download the tar.gz file from github like so
-#       `curl -O -L "https://github.com/Codeon-GmbH/mulle-clang/archive/4.0.0.4.tar.gz"`
-#    3. Run shasum over it `shasum -a 256 -b 4.0.0.4.tar.gz`
+#       `curl -O -L "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"`
+#    3. Run shasum over it `shasum -a 256 -b 5.0.0.0.tar.gz`
 #    4. Remove bottle urls
 #
-   url "https://github.com/Codeon-GmbH/mulle-clang/archive/4.0.0.4.tar.gz"
-   sha256 "135ac25be678afe1927dbb3b65fd6be6edb5a811c5291836423cc24bd6c703a6"
-#   sha256 "a59bf02dbb6810ea546e9f4cb99adee0a271b58954a1ed825a393d411a719bf1"
+   url "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"
+   sha256 "4df6b9ef00118b81aa6c73f1fe9c3f07376d064342ad45299144537840844992"
 
 #
 # MEMO:
@@ -28,16 +27,16 @@ class MulleClang < Formula
   bottle do
 #    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
     root_url "http://download.codeon.de/bottles"
-    sha256 "a41d43786789cb206d5b42a3c942d0bb5db373dbc8b31760f8e768ab4236f20c" => :yosemite
-    sha256 "9a5d0889a9cc329a0cf065ff107e03fe82ce4f0e99627004b468ed190b4eac3c" => :el_capitan
-    sha256 "8db93d220d0b0d3848ca3ff30666100213d1b3c24c4ab953d477388cb91afc4c" => :sierra
+#    sha256 "a41d43786789cb206d5b42a3c942d0bb5db373dbc8b31760f8e768ab4236f20c" => :yosemite
+#    sha256 "9a5d0889a9cc329a0cf065ff107e03fe82ce4f0e99627004b468ed190b4eac3c" => :el_capitan
+#    sha256 "8db93d220d0b0d3848ca3ff30666100213d1b3c24c4ab953d477388cb91afc4c" => :sierra
     cellar :any
   end
 
    # actually depends on llvm39, but versioning is tricky in homebrew
    # probably need to change PATH below too, when llvm moves to 40
 
-   depends_on 'llvm@4'  => :build
+   depends_on 'llvm@5'  => :build
    depends_on 'cmake'   => :build
 
    #
