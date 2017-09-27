9840fd934687618b554adf0ab74e5bc4e5a3236d
Nat! <nat@codeon.de>
2017-09-27 11:30:43 +0200

    add bottle and caveats

--- mulle-clang-homebrew.rb
+++ mulle-clang-homebrew.rb
@@ -1,40 +1,48 @@
 class MulleClangHomebrew < Formula
-   homepage "https://github.com/Codeon-GmbH/mulle-clang-homebrew"
-   desc "Shim for compiling homebrew packages with the mulle-objc compiler"
+  homepage "https://github.com/Codeon-GmbH/mulle-clang-homebrew"
+  desc "Shim for compiling homebrew packages with the mulle-objc compiler"
 
-   depends_on 'mulle-clang'   => :build
+  depends_on 'mulle-clang' => :build
 
-   url "https://github.com/Codeon-GmbH/mulle-clang-homebrew/archive/0.0.1.tar.gz"
-   sha256 "2e9364a8606dce0f9697aa368660648185153fb6a10ab3b2e2cfc02908005168"
+  url "https://github.com/Codeon-GmbH/mulle-clang-homebrew/archive/0.0.1.tar.gz"
+  sha256 "2e9364a8606dce0f9697aa368660648185153fb6a10ab3b2e2cfc02908005168"
 
-   #
-   # homebrew llvm is built with polly, but cmake doesn't pick it up
-   # for some reason
-   #
-   def install
-      shimdir = ENV[ "HOMEBREW_LIBRARY"] + "/Homebrew/shims/super"
-      src     = shimdir + "/cc"
-      dst     = "#{prefix}/bin/mulle-clang"
+  #
+  # homebrew llvm is built with polly, but cmake doesn't pick it up
+  # for some reason
+  #
+  def install
+    shimdir = ENV[ "HOMEBREW_LIBRARY"] + "/Homebrew/shims/super"
+    src     = shimdir + "/cc"
+    dst     = "#{prefix}/bin/mulle-clang"
 
-      if ! File.directory?( shimdir)
-        raise StandardError, "Unable to find homebrew shimdir " + shimdir
-      end
+    if ! File.directory?( shimdir)
+      raise StandardError, "Unable to find homebrew shimdir " + shimdir
+    end
 
-      if ! File.readable?( src)
-        raise StandardError, "Unable to find homebrew shim cc in shimdir " + shimdir
-      end
+    if ! File.readable?( src)
+      raise StandardError, "Unable to find homebrew shim cc in shimdir " + shimdir
+    end
 
-      if ! File.directory?( "#{prefix}/bin")
-         Dir.mkdir( "#{prefix}/bin", 0755)
-      end
+    if ! File.directory?( "#{prefix}/bin")
+       Dir.mkdir( "#{prefix}/bin", 0755)
+    end
 
-      text = File.read( src)
-      text = text.gsub( /\/\^clang\//, "/clang/")
-      File.open( dst, "w") {|file| file.puts text }
-      File.chmod( 0755, dst)
-   end
+    text = File.read( src)
+    text = text.gsub( /\/\^clang\//, "/clang/")
+    File.open( dst, "w") {|file| file.puts text }
+    File.chmod( 0755, dst)
+  end
 
-   test do
-      true
-   end
+  test do
+     true
+  end
+
+  def caveats
+    str = <<-EOS.undent
+    The warning about the non-installable link is unavoidable. Please ignore
+    it and do NOT relink!
+    EOS
+    str
+  end
 end
--- mulle-clang.rb
+++ mulle-clang.rb
@@ -1,6 +1,6 @@
 class MulleClang < Formula
-   homepage "https://github.com/Codeon-GmbH/mulle-clang"
-   desc "Objective-C compiler for the mulle-objc runtime"
+  homepage "https://github.com/Codeon-GmbH/mulle-clang"
+  desc "Objective-C compiler for the mulle-objc runtime"
 #
 # MEMO:
 #
@@ -10,8 +10,8 @@ class MulleClang < Formula
 #    3. Run shasum over it `shasum -a 256 -b 5.0.0.0.tar.gz`
 #    4. Remove bottle urls
 #
-   url "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"
-   sha256 "ce81b6b32aa2990cf386d4ebe05be7893c1f311c8fac5834c73a37ea4a75f3ce"
+  url "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"
+  sha256 "ce81b6b32aa2990cf386d4ebe05be7893c1f311c8fac5834c73a37ea4a75f3ce"
 
 #
 # MEMO:
@@ -30,42 +30,48 @@ class MulleClang < Formula
 #    sha256 "a41d43786789cb206d5b42a3c942d0bb5db373dbc8b31760f8e768ab4236f20c" => :yosemite
     sha256 "5f5aad6f45f242e6f8fe5dd1d1122f8855e122e2b56dafff69af451364d04c29" => :el_capitan
     sha256 "c0f6e19a618a569f8ab9000b2857cb7bd43f6f4df300b6ff19886618a36966d8" => :sierra
+    sha256 "ff8bdba34a3ed7deae363b595f0105fcb75d2651466f491a6d258c20e95b7030" => :high_sierra
     cellar :any
   end
 
    # actually depends on llvm39, but versioning is tricky in homebrew
    # probably need to change PATH below too, when llvm moves to 40
 
-   depends_on 'llvm@5'  => :build
-   depends_on 'cmake'   => :build
-   depends_on 'ninja'   => :build
+  depends_on 'llvm@5'  => :build
+  depends_on 'cmake'   => :build
+  depends_on 'ninja'   => :build
 
-   #
-   # homebrew llvm is built with polly, but cmake doesn't pick it up
-   # for some reason
-   #
-   def install
-      mkdir "build" do
-         args = std_cmake_args
-         args << "-DCMAKE_INSTALL_PREFIX=#{prefix}/root"
-         args << "-DLINK_POLLY_INTO_TOOLS=ON"
-         args << "-DCMAKE_EXE_LINKER_FLAGS=-lPolly -lPollyISL"
-         args << ".."
-         ENV["PATH"] = "/usr/local/opt/llvm/bin" + File::PATH_SEPARATOR + ENV["PATH"]
-         system "cmake", "-G", "Ninja", *args
-         system "ninja", "install"
+  #
+  # homebrew llvm is built with polly, but cmake doesn't pick it up
+  # for some reason
+  #
+  def install
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}/root"
+      args << "-DLINK_POLLY_INTO_TOOLS=ON"
+      args << "-DCMAKE_EXE_LINKER_FLAGS=-lPolly -lPollyISL"
+      args << ".."
+      ENV["PATH"] = "/usr/local/opt/llvm/bin" + File::PATH_SEPARATOR + ENV["PATH"]
+      system "cmake", "-G", "Ninja", *args
+      system "ninja", "install"
 
-         bin.install_symlink "#{prefix}/root/bin/clang" => "mulle-clang"
-         bin.install_symlink "#{prefix}/root/bin/scan-build" => "mulle-scan-build"
-         bin.install_symlink "#{prefix}/root/share/clang/mulle-clang-add-brew-post-checkout-hook" => "mulle-clang-add-brew-post-checkout-hook"
+      bin.install_symlink "#{prefix}/root/bin/clang" => "mulle-clang"
+      bin.install_symlink "#{prefix}/root/bin/scan-build" => "mulle-scan-build"
+      bin.install_symlink "#{prefix}/root/share/clang/mulle-clang-add-brew-post-checkout-hook" => "mulle-clang-add-brew-post-checkout-hook"
+    end
+  end
 
-         opoo "To enable mulle-clang to be used in homebrew formulae, you"
-         opoo "must add add a git post-checkout hook to brew. To do this run:"
-         opoo "   mulle-clang-add-brew-post-checkout-hook"
-      end
-   end
-
-   test do
-      system "#{bin}/mulle-clang", "--help"
-   end
+  def caveats
+    str = <<-EOS.undent
+    To build homebrew packages with mulle-clang, you need to add the following
+    lines to your formula:
+       depends_on 'mulle-clang-homebrew' => :build
+       depends_on 'mulle-clang' => :build
+    EOS
+    str
+  end
+  test do
+    system "#{bin}/mulle-clang", "--help"
+  end
 end
