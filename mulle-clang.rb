4e99355765476a5eb91f866e1ff957702b06f9e0
Nat! <nat@codeon.de>
2017-10-04 12:41:47 +0200

    update to 5.0.0.1

--- mulle-clang.rb
+++ mulle-clang.rb
@@ -10,27 +10,28 @@ class MulleClang < Formula
 #    3. Run shasum over it `shasum -a 256 -b 5.0.0.0.tar.gz`
 #    4. Remove bottle urls
 #
-  url "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"
-  sha256 "ce81b6b32aa2990cf386d4ebe05be7893c1f311c8fac5834c73a37ea4a75f3ce"
+  url "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.1.tar.gz"
+  sha256 "a88db21f9e8e41d6684074bea3c22a3765141aa9b947f11979df04dd228b90f2"
+
+  def vendor
+    "mulle-clang 5.0.0.1 (runtime-load-version: 11)"
+  end
 
 #
 # MEMO:
 #    For each OS X version, create bottles with:
 #
 #    `brew uninstall codeon-gmbh/software/mulle-clang`
-#    `brew install --build-bottle codeon-gmbh/software/mulle-clang`
-#    `brew bottle codeon-gmbh/software/mulle-clang`
-#
-#     Unfortunately building from a local recipe with file:/// doesn't work
+#    `brew install --build-bottle --build-from-source mulle-clang.rb`
+#    `brew bottle --force-core-tap --build-from-source mulle-clang.rb
 #
 
   bottle do
 #    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
     root_url "http://download.codeon.de/bottles"
-#    sha256 "a41d43786789cb206d5b42a3c942d0bb5db373dbc8b31760f8e768ab4236f20c" => :yosemite
-    sha256 "5f5aad6f45f242e6f8fe5dd1d1122f8855e122e2b56dafff69af451364d04c29" => :el_capitan
-    sha256 "c0f6e19a618a569f8ab9000b2857cb7bd43f6f4df300b6ff19886618a36966d8" => :sierra
-    sha256 "ff8bdba34a3ed7deae363b595f0105fcb75d2651466f491a6d258c20e95b7030" => :high_sierra
+    sha256 "ace664e414470cfc9c5696a6f94bf829def818cc40167c44fa1fc31571e58196" => :sierra
+#    sha256 "5f5aad6f45f242e6f8fe5dd1d1122f8855e122e2b56dafff69af451364d04c29" => :el_capitan
+#    sha256 "ff8bdba34a3ed7deae363b595f0105fcb75d2651466f491a6d258c20e95b7030" => :high_sierra
     cellar :any
   end
 
@@ -46,8 +47,10 @@ class MulleClang < Formula
   # for some reason
   #
   def install
+
     mkdir "build" do
       args = std_cmake_args
+      args << "-DCLANG_VENDOR="#{vendor}"
       args << "-DCMAKE_INSTALL_PREFIX=#{prefix}/root"
       args << "-DLINK_POLLY_INTO_TOOLS=ON"
       args << "-DCMAKE_EXE_LINKER_FLAGS=-lPolly -lPollyISL"
