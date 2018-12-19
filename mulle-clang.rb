7cb8801f777f555faae79e6057b7adc95dfc5054
Nat! <nat@codeon.de>
2018-12-19 11:47:41 +0100

    7.0.0.0 release of mulle-clang

--- mulle-clang.rb
+++ mulle-clang.rb
@@ -6,16 +6,16 @@ class MulleClang < Formula
 #
 #    1. Create a release on github
 #    2. Download the tar.gz file from github like so
-#       `curl -O -L "https://github.com/Codeon-GmbH/mulle-clang/archive/5.0.0.0.tar.gz"`
-#    3. Run shasum over it `shasum -a 256 -b 5.0.0.0.tar.gz`
+#       `curl -O -L "https://github.com/Codeon-GmbH/mulle-clang/archive/7.0.0.0.tar.gz"`
+#    3. Run shasum over it `shasum -a 256 -b 7.0.0.0.tar.gz`
 #    4. Remove bottle urls
 #
-  url "https://github.com/Codeon-GmbH/mulle-clang/archive/6.0.0.6.tar.gz"
-  sha256 "38c72bec447e1bf9b224ebf499d1f62a9f7ed84c3bb55e6a98d2ca78136eb2f1"
+  url "https://github.com/Codeon-GmbH/mulle-clang/archive/7.0.0.0.tar.gz"
+  sha256 "b402c790e48095191c5159a9dcc1890b793e62abf03b8563852027c1d951f638"
 
 
   def vendor
-    "mulle-clang 6.0.0.6 (runtime-load-version: 12)"
+    "mulle-clang 7.0.0.0 (runtime-load-version: 14)"
   end
 
 #
@@ -34,13 +34,16 @@ class MulleClang < Formula
 #    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
     root_url "http://download.codeon.de/bottles"
 
-    sha256 "30fac5ce44ded69213186af78b4538d6d55188add5428045a69276e3546b393a" => :high_sierra
-    sha256 "e2b4a3db668a9f7c7b68eae24975f98a33d5d1a14f8cecebc663cf33e023c11a" => :sierra
-
+       sha256 "5e5ada04a69ff49a2311d2fac9d3856c2e63b2b1712c1ed4e684c1e083198cdc" => :mojave
+       sha256 "0f111c4eb324aa9fc3fdad7b2b530e0cca8a29186bfb2e53a948170fffa8aa3a" => :high_sierra
     cellar :any
   end
 
-  depends_on 'llvm@6'  => :build
+#
+# MEMO:
+#    Change llvm to proper version
+#
+  depends_on 'llvm@7'  => :build
   depends_on 'cmake'   => :build
   depends_on 'ninja'   => :build
 
